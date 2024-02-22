680462955a1e96c40ce32cd141d1509fa416e7e2
EddieGong <superxiaogong@msn.com>
2024-02-22 20:13:03 +0100

    Add DEMO h/cpp

---
+++ DEMO/Source/DEMO.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace ZERO
+{
+    class DEMO
+    {
+    public:
+        class DefualtConfig
+        {
+        public:
+            static constexpr int width  = 800;
+            static constexpr int height = 600;
+        };
+    };
+}

bffd732ed25bd9c55ed593c442c660b242deef7f
EddieGong <superxiaogong@msn.com>
2024-02-23 16:51:28 +0100

    Add c++ unit test project

---
+++ DEMO/DEMO.Tests/ArithmeticBasicsTests.cpp
@@ -0,0 +1,16 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ZERO
+{
+	TEST_CLASS(ArithmeticBasicTests)
+	{
+	public:
+		
+		TEST_METHOD(TestArithmeticBasics)
+		{
+			Logger::WriteMessage("In TestArithmeticBasics");
+		}
+	};
+}
---
+++ DEMO/DEMO.Tests/LogicalBasicsTests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ZERO
+{
+	TEST_CLASS(LogicalBasicsTests)
+	{
+	public:
+
+		TEST_METHOD(TestLogicalBasics)
+		{
+		}
+	};
+}

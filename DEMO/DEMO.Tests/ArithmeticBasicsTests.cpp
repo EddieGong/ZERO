2b88bf984295107061d5fa16269157cc151beec3
EddieGong <superxiaogong@msn.com>
2024-02-25 22:23:35 +0100

    Add code for testing arithmetic module

--- DEMO/DEMO.Tests/ArithmeticBasicsTests.cpp
+++ DEMO/DEMO.Tests/ArithmeticBasicsTests.cpp
@@ -1,4 +1,5 @@
 #include "CppUnitTest.h"
+import ArithmeticBasics;
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,6 +12,18 @@ namespace ZERO
 		TEST_METHOD(TestArithmeticBasics)
 		{
 			Logger::WriteMessage("In TestArithmeticBasics");
+
+			int sum = ZERO::Arithmetic::Sum(1, 2, 3, 4, 5);
+			Assert::AreEqual(15, sum);
+
+			int square = ZERO::Arithmetic::Square(7);
+			Assert::AreEqual(49, square);
+
+			int evenCount = ZERO::Arithmetic::CountEven(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+			Assert::AreEqual(5, evenCount);
+
+			int factorial = ZERO::Arithmetic::Factorial(5);
+			Assert::AreEqual(120, factorial);
 		}
 	};
 }

64767feb31e040e23df4f42f0b6b4341ae90c6ab
EddieGong <superxiaogong@msn.com>
2024-02-28 22:00:46 +0100

    Add integer type module

--- DEMO/DEMO.Tests/ArithmeticBasicsTests.cpp
+++ DEMO/DEMO.Tests/ArithmeticBasicsTests.cpp
@@ -1,5 +1,7 @@
 #include "CppUnitTest.h"
+
 import ArithmeticBasics;
+import IntegerTypes;
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,19 +13,24 @@ namespace ZERO
 		
 		TEST_METHOD(TestArithmeticBasics)
 		{
+			using IntegerTypes::int32;
+			using IntegerTypes::uint16;
+			using IntegerTypes::uint32;
+			using IntegerTypes::uint64;
+
 			Logger::WriteMessage("In TestArithmeticBasics");
 
-			int sum = ZERO::Arithmetic::Sum(1, 2, 3, 4, 5);
+			int32 sum = Arithmetic::Sum(1, 2, 3, 4, 5);
 			Assert::AreEqual(15, sum);
 
-			int square = ZERO::Arithmetic::Square(7);
-			Assert::AreEqual(49, square);
+			uint64 square = Arithmetic::Square(7);
+			Assert::AreEqual(uint64(49), square);
 
-			int evenCount = ZERO::Arithmetic::CountEven(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+			int32 evenCount = Arithmetic::CountEven(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 			Assert::AreEqual(5, evenCount);
 
-			int factorial = ZERO::Arithmetic::Factorial(5);
-			Assert::AreEqual(120, factorial);
+			uint32 factorial = Arithmetic::Factorial(5);
+			Assert::AreEqual(uint32(120), factorial);
 		}
 	};
 }
